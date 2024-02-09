9af66ea9354e473a0b05973862c6ffc2c7ff7c0b
Nader Fares <naderalifares16@hotmail.com>
2024-02-08 23:17:21 -0300

    Creating CRUD

--- app.py
+++ app.py
@@ -31,3 +31,49 @@ def index():
   patients = cursor.fetchall()
   conn.close()
   return render_template('index.html', patients=patients)
+
+@app.route('/newPatient', methods=['GET', 'POST'])
+def newPatient():
+  if request.method == 'POST':
+    name = request.form['name']
+    age = request.form['age']
+    gender = request.form['gender']
+    document = request.form['document']
+    address = request.form['address']
+    phone = request.form['phone']
+
+    conn = sqlite3.connect('hospital_management.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+      INSERT INTO patients (
+        name,
+        age,
+        gender,
+        document,
+        address,
+        phone
+      ) VALUES (
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?
+      )
+    ''', (name, age, gender, document, address, phone))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+  return render_template('newPatient.html')
+
+@app.route('/eraseHistory')
+def eraseHistory():
+  conn = sqlite3.connect('hospital_management.db')
+  cursor = conn.cursor()
+  cursor.execute('DELETE FROM patients')
+  conn.commit()
+  conn.close()
+  return redirect(url_for('index'))
+
+if __name__ == '__main__':
+  app.run(debug=True)
